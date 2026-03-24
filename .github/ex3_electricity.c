8bce095d35ceb9c507f876990950e6dbc863df11
Edrine Allan <aangedrindavinci@gmail.com>
2026-03-24 17:34:08 +0300

    Fixed a comment
    Added comments to clarify variable declarations, input prompts, calculations, and output.

--- .github/ex3_electricity.c
+++ .github/ex3_electricity.c
@@ -3,15 +3,17 @@
 
 #include <stdio.h>
 int main(){
+//declaring variables
     float units, cost_per_unit, total_bill;
-
+//enter units consumed
     printf("Enter units consumed: ");
     scanf("%f", &units);
-
+//enter cost per unit
     printf("Enter cost per unit: UGX ");
     scanf("%f", &cost_per_unit);
-
+//calculate total bill
     total_bill = (units * cost_per_unit);
+//display total bill
     printf("Total bill: %.2f UGX\n", total_bill);
 
     return 0;
